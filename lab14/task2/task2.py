5c732a84195adf20e3925f275b458c356e3acdee
mizmor <mizmorrr@gmail.com>
2022-10-03 00:29:33 +0300

    task2 - done

--- lab14/task2/task2.py
+++ lab14/task2/task2.py
@@ -1,10 +1,32 @@
+from random import randint
 import tkinter
 keys = open("task2/keys.txt","r")
 vals = open("task2/vals.txt","r")
 lkeys = keys.readlines()
 lvals=vals.readlines()
+
 def btnclick():
-    mes.pack()
+    if counter.get()<3:
+        wd=word.get().rstrip().lstrip().lower()
+        if wd==d[list(d.keys())[i]]:
+            mes.configure(text="Верно",fg='green')
+            btn.configure(bg="green")
+            btn.configure(state='disabled')
+            counter.set(0)
+            
+        else:
+            btn.configure(bg="red")
+            mes.configure(text="Неверно!",fg='red')
+            counter.set(counter.get()+1)
+        mes.place(x=205, y = 160)
+    else:
+        mes.configure(text="Ты проиграл!",fg='black',bg='red')
+        mes.place(x=205, y = 160)
+        btn.configure(state='disabled')
+def surrender():
+    btn.configure(state='disabled')
+    mes.configure(text="Слово - "+str(d[list(d.keys())[i]]),width=250,fg='red')
+    mes.place(x=170, y = 160)
 def cleart(a): 
     return (a.replace("\t",' '))
 def strformatter(a):
@@ -23,12 +45,29 @@ for i in range(len(lkeys)):
         d[postkeys[j]]=postvals[j]
         
 
-window=tkinter.Tk()
-mes = tkinter.Message(window,text="message",width=50)
-
-
+window=tkinter.Tk(className="task")
+window.geometry("500x250")
+mes = tkinter.Message(window,text="Верно!",width=100,fg='green',font=('Arial,15'))
+i=randint(0,len(list(d.keys()))-1)
+var = tkinter.StringVar()
+var="Слово: "+list(d.keys())[i]
+rightness="test"
+lab=tkinter.Label(text=var, font=('Arial', 15),width=50,height=3)
+lab.pack()
 frame = tkinter.Frame(window)
 frame.pack()
-btn=tkinter.Button(frame,command=btnclick)
-btn.pack()
+btn=tkinter.Button(frame,command=btnclick,width=10,height=2,text="try",font=('Arial',15, 'bold'))
+btn.pack(side=tkinter.TOP)
+word =tkinter.Entry(window,width=20)
+word.place(x=325,y=112)
+lab2=tkinter.Label(text="Перевод: ", font=('Arial', 15,'italic'))
+lab2.place(x=325,y=70)
+counter=tkinter.IntVar()
+counter.set(0)
+lab3=tkinter.Label(window,textvariable=counter,width=1,font=('Arial', 11))
+lab3.place(x=150,y=100)
+lab4=tkinter.Label(window,text ='Попытка: ',font=('Arial', 11))
+lab4.place(x=70, y =100)
+btnsur=tkinter.Button(window,command=surrender,width=5,height=2,text="сдаюсь",font=('Arial',13, 'bold'),bg='red')
+btnsur.place(x=0,y=0)
 window.mainloop()
