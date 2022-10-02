2fde78e42342e2d72e2c07e25f99cef757322198
mizmor <mizmorrr@gmail.com>
2022-10-02 21:49:03 +0300

    l

---
+++ lab1.py
@@ -0,0 +1,9 @@
+fst = int (input ('fst = '))
+snd = int (input ('snd = '))
+thrd = int (input ('thrd = '))
+
+res = fst + snd +thrd
+
+if  res%2==0: 
+    print ('result = ',res/2)
+else: print('result = ',res/2+0.5) 
